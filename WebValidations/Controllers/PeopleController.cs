e6a0f05aaa897c036382a4da281119d75cb4e2a9
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-01-24 21:29:22 -0300

    Add project files.

---
+++ WebValidations/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebValidations.Models;
+
+namespace WebValidations.Controllers
+{
+   public class PeopleController : Controller
+   {
+      public IActionResult Index()
+      {
+         return View();
+      }
+
+      public IActionResult Create()
+      {         
+         return View();
+      }
+
+      [HttpPost]
+      public IActionResult Create(People model)
+      {
+         if (ModelState.IsValid)
+         {
+            //validou
+            return RedirectToAction(nameof(Index));
+         }
+         return View(model);
+      }
+   }
+}
---
+++ WebValidations/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebValidations.Models
+{
+   public class People
+   {
+      [Required(ErrorMessage = "Digite o Id")]
+      public int Id { get; set; }
+
+      [Required(ErrorMessage = "Digite o nome")]
+      public string Name { get; set; }
+   }
+}
